b1dbc262987531b9c6eb643ea405ed822a88784e
Dinesh Pinto <annual.fallout_0z@icloud.com>
2024-11-07 10:31:16 +0400

    feat: add deployment script

--- script/FlareVtpmAttestation.s.sol
+++ script/FlareVtpmAttestation.s.sol
@@ -1,18 +1,47 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
 import {FlareVtpmAttestation} from "../contracts/FlareVtpmAttestation.sol";
+import {OidcSignatureVerification} from "../contracts/verifiers/OidcSignatureVerification.sol";
 import {Script, console} from "forge-std/Script.sol";
 
 contract FlareVtpmAttestationScript is Script {
-    FlareVtpmAttestation public flareVtpm;
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    string hwmodel = vm.envString("HWMODEL");
+    string swname = vm.envString("SWNAME");
+    string imageDigest = vm.envString("IMAGE_DIGEST");
+    string iss = vm.envString("ISS");
+    bool secboot = vm.envBool("SECBOOT");
 
-    function setUp() public {}
+    FlareVtpmAttestation flareVtpm;
+    OidcSignatureVerification oidcVerifier;
 
-    function run() public {
-        vm.startBroadcast();
+    function deploy() public {
+        // Starting the broadcast of transactions from the deployer account
+        vm.startBroadcast(deployerPrivateKey);
 
+        // Deploy the FlareVtpmAttestation contract
         flareVtpm = new FlareVtpmAttestation();
+        console.log("FlareVtpmAttestation deployed at:", address(flareVtpm));
+
+        // Set base configuration on the deployed contract
+        flareVtpm.setBaseQuoteConfig(hwmodel, swname, imageDigest, iss, secboot);
+
+        // Log that the base configuration has been set
+        console.log("Base quote configuration set with:");
+        console.log("  Hardware Model:", hwmodel);
+        console.log("  Software Name:", swname);
+        console.log("  Image Digest:", imageDigest);
+        console.log("  Issuer:", iss);
+        console.log("  Secure Boot:", secboot);
+
+        // Deploy the OidcSignatureVerification contract
+        oidcVerifier = new OidcSignatureVerification();
+        console.log("OidcSignatureVerification deployed at:", address(oidcVerifier));
+
+        // Set the token type verifier to the OidcSignatureVerification contract
+        flareVtpm.setTokenTypeVerifier(address(oidcVerifier));
+        console.log("FlareVtpmAttestation token type verifier set to OidcSignatureVerification");
 
         vm.stopBroadcast();
     }
